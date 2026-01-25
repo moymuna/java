69b8cdbeee175d48524b0e53f6c6994d2313ac7e
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2026-01-25 18:49:44 +0600

    add

---
+++ NetBeansProjects/computAreaJava/src/computareajava/ComputAreaJava.java
@@ -0,0 +1,43 @@
+
+package computareajava;
+
+import java.util.Scanner;
+
+
+public class ComputAreaJava {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println("enter second");
+//        int seconds =scanner.nextInt();
+//        int minutes =seconds/60;
+//        int second =seconds%60;
+//        System.out.println(minutes+" minuts "+second+" seconds");
+        
+       
+//        Scanner input =new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String name= input.nextLine();
+//        System.out.println("my name is " + name);
+      
+       
+//        Scanner input= new Scanner(System.in);
+//        float area;
+//        System.out.println("enter radius");
+//        float radius = input.nextFloat();
+//        area=(float)3.1416 *radius*radius;
+//        System.out.println("area of acircle is "+ area);
+       
+        
+//        float radius;
+//        float area;
+//        radius = 5;
+//        area= (float)3.1416*radius*radius;
+//        System.out.println("area of this circle is"+area);
+    }
+    
+}
