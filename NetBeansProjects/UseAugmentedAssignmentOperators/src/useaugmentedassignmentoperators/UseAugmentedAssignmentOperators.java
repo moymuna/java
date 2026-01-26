faf23d30677d8fa156deffeea7d8df7d9f95ca8e
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2026-01-26 18:57:44 +0600

    add

---
+++ NetBeansProjects/UseAugmentedAssignmentOperators/src/useaugmentedassignmentoperators/UseAugmentedAssignmentOperators.java
@@ -0,0 +1,63 @@
+
+package useaugmentedassignmentoperators;
+
+import java.util.Scanner;
+
+
+public class UseAugmentedAssignmentOperators {
+
+    
+    public static void main(String[] args) {
+//        int n1 = 10;
+//        System.out.println(n1);
+//        System.out.println(n1+=8);
+//        System.out.println("n1+=8 = "+ n1 );
+//        System.out.println(n1-=8);
+//        System.out.println("n1-=8 = "+n1);
+//        System.out.println(n1*=8);
+//        System.out.println("n1*=8 = "+ n1);
+//        System.out.println(n1/=8);
+//        System.out.println("n1/=8 = "+ n1);
+//        System.out.println(n1%=8);
+//        System.out.println("n1%=8 = "+ n1);
+
+
+//        
+//        int n1 = 3;
+//        System.out.println("n1 = " + n1);
+//        int n2 = n1++;
+//        System.out.println("n2 is " + n2);
+//        int n3 = n2--;
+//        System.out.println("n3 is " + n3);
+//        int n4 = ++n1;
+//        System.out.println("n4 is " + n4);
+//        int n5 = --n1;
+//        System.out.println("n5 is " + n5);
+
+                
+//        int a = 5;
+//        System.out.println("A = " + a);
+//        System.out.println("A++ = " + a++);
+//        System.out.println("A = " + a);
+//        System.out.println("++A = " + ++a);
+//        System.out.println("A = " + a);
+//        System.out.println("--A = " + --a);
+//        System.out.println("A = " + a);
+//        System.out.println("A-- = " + a--);
+//        System.out.println("A = " + a);
+
+
+//        Scanner number1 = new Scanner(System.in);
+//        System.out.println("input first number");
+//        int n1 = number1.nextInt();
+//        System.out.println("input second number");
+//        int n2 = number1.nextInt();
+//        System.out.println(n1 + n2);
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("What is your name");
+//        String name = input.nextLine();
+//        System.out.println("Welcome " + name);
+    }
+    
+}
