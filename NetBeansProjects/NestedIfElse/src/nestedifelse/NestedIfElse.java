7d71bce8a2f473545d7d1ed4deb2bd8d11542e8a
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2026-01-28 19:00:40 +0600

    add

---
+++ NetBeansProjects/NestedIfElse/src/nestedifelse/NestedIfElse.java
@@ -0,0 +1,95 @@
+package nestedifelse;
+
+import java.util.Scanner;
+
+public class NestedIfElse {
+
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter your username");
+//        String username = input.nextLine();
+//        if (username.equalsIgnoreCase("java")) {
+//            System.out.println("Enter your password");
+//            String password = input.nextLine();
+//            if (password.equals("abC123")) {
+//                System.out.println("welcome java");
+//            } else {
+//                System.err.println("incorrect password");
+//            }
+//        } else {
+//            System.err.println("username is incorrect");
+//        }
+
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("input BMI");
+//        float bmi = input.nextFloat();
+//        if (bmi <= 18.5) {
+//            System.out.println("underweight");
+//        } else if (bmi <= 25.0) {
+//            System.out.println("normal");
+//        } else if (bmi < 30.0) {
+//            System.out.println("overweight");
+//        } else {
+//            System.out.println("obese");
+//        }
+
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("inter number");
+//        int number = input.nextInt();
+//
+//        if (number % 2 == 0) {
+//            System.out.println("even number");
+//        } else {
+//            System.out.println("odd number");
+//        }
+
+
+
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("enter first number");
+//        float n1 = input.nextFloat();
+//        System.out.println("enter + ,-,*,/");
+//        String oparator = input.next();
+//        System.out.println("enter second number");
+//        float n2 = input.nextFloat();
+//        if (oparator.equals("+")) {
+//            System.out.println(n1 + n2);
+//        } else if (oparator.equals("-")) {
+//            System.out.println(n1 - n2);
+//        } else if (oparator.equals("*")) {
+//            System.out.println(n1 * n2);
+//        } else if (oparator.equals("/")) {
+//            System.out.println(n1 / n2);
+//        }
+//          switch (oparator) {
+//            case "+":
+//                System.out.println(n1 + n2);
+//                break;
+//            case "-":
+//                System.out.println(n1 - n2);
+//                break;
+//            case "*":
+//                System.out.println(n1 * n2);
+//                break;
+//            case "/":
+//                System.out.println(n1 / n2); 
+//}
+    
+
+
+
+
+
+
+
+
+    }
+
+}
